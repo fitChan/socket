97c88ca0072bbbfe3522d5b2fdbc47a5e3c7068c
fitChan <84306157+fitChan@users.noreply.github.com>
2023-04-23 04:16:56 +0900

    각각 방별 소켓 연결

--- index.js
+++ index.js
@@ -11,15 +11,12 @@ const io = require('socket.io')(server, {
 
 // 클라이언트가 접속하면 실행
 io.on('connection', (socket) => {
-  console.log('a user connected');
 
   // 클라이언트로부터 메시지를 받으면 실행
-  socket.on('set_ur_emit_key', (this_is_msg_content) => {
-    console.log('message: ' + this_is_msg_content);
-    console.log("채팅 보냇네 이제 redis연결해서 다시 온라인값으로 바꾸면돼")
-
-    // 모든 클라이언트에게 메시지 전송
-    io.emit('send_to_ur_audience', this_is_msg_content);
+   socket.on('set_ur_emit_key', (this_is_msg_content) => {
+    console.log('message: ' + this_is_msg_content.input);
+    socket.join(this_is_msg_content.room); // join socket to the room
+    io.to(this_is_msg_content.room).emit('send_to_ur_audience', this_is_msg_content);
   });
 
   // 클라이언트가 접속을 종료하면 실행
@@ -31,7 +28,7 @@ io.on('connection', (socket) => {
 
   // 클라이언트로부터 disconnect_me 이벤트를 받으면 연결 끊기
   socket.on('disconnect_me', (reason) => {
-    console.log('10초 지났어 여기서 redis연결해서 오프라인값 저장하면 되겠지?');
+    // console.log('10초 지났어 여기서 redis연결해서 오프라인값 저장하면 되겠지?');
   });
 });
 
